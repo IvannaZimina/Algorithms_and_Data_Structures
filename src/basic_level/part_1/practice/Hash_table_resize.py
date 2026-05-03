ee7624ceebbdcb7d7479874778acf3ca462b04d7
Zimina Ivanna <ivanna.zimina@plus4u.net>
2026-05-03 16:35:38 +0300

    Add linear probing resize hash table

--- src/basic_level/part_1/practice/Hash_table_resize.py
+++ src/basic_level/part_1/practice/Hash_table_resize.py
@@ -4,6 +4,7 @@ class HashData:
         self.size = 10
         self.table = [None] * self.size
         self.lf_threshold = 0.7
+        self.count = 0
 
     # compute hash
     def hash_function(self, key):
@@ -12,17 +13,36 @@ class HashData:
     # insert data to hash table
     def put(self, key):
         # insert into table
+        index = self.hash_function(key)
+        while self.table[index] is not None:
+            index = (index + 1) % self.size
+        self.table[index] = key
+        self.count += 1
 
         # compute threshold during each insertion
         # current lf = occupied slot/total slots
+        load_factor = self.count / self.size
 
         # check if load factor exceeds after each insertion
+        if load_factor >= self.lf_threshold:
+            self.rehash()
 
 
     def rehash(self):
         # create a new hash table
+        old_table = self.table
+        self.size *= 2
+        self.table = [None] * self.size
+        self.count = 0
 
         # hash existing data into new table
+        for key in old_table:
+            if key is not None:
+                index = self.hash_function(key)
+                while self.table[index] is not None:
+                    index = (index + 1) % self.size
+                self.table[index] = key
+                self.count += 1
 
         # update table
 
